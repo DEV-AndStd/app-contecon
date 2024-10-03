e49755af71d86ba052dca6b521691f52d0bb1d9d
DEV-AndStd <jordan-medina05@hotmail.com>
2024-10-03 13:14:52 -0600

    path

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const equiposRoutes = require('./routes/equipos');
 const repuestosRoutes = require('./routes/repuestos');
 const usuariosRoutes = require('./routes/usuarios');
