fe6503189121a7810d16e3a78cc4a01b92917784
DEV-AndStd <jordan-medina05@hotmail.com>
2024-10-03 11:54:39 -0600

    datos

--- routes/exportar.js
+++ routes/exportar.js
@@ -6,32 +6,33 @@ router.get('/', async (req, res) => {
     try {
         const result = await pool.query('SELECT * from usuarios'); 
         const data = result.rows;
-
+    
         // Obtener los encabezados
-    const headers = Object.keys(data[0]).join(','); // Sin comillas alrededor de los encabezados
-
-    // Convierte los datos a CSV, añadiendo comillas para manejar comas en los datos
-    const csvRows = data.map(row => {
-        return Object.values(row)
-            .map(value => {
-                // Envolver el valor en comillas si contiene comas o comillas dobles
-                const escapedValue = value.toString().replace(/"/g, '""');
-                return escapedValue.includes(',') || escapedValue.includes('"') ? `"${escapedValue}"` : escapedValue;
-            })
-            .join(','); // Usar coma como separador
-    });
-
-    // Combinar encabezados y datos
-    const csv = [headers, ...csvRows].join('\n');
-
+        const headers = Object.keys(data[0]).join(','); // Encabezados separados por comas
+    
+        // Convierte los datos a CSV, añadiendo comillas para manejar comas en los datos
+        const csvRows = data.map(row => {
+            return Object.values(row)
+                .map(value => {
+                    // Escapar comillas dobles
+                    const escapedValue = value.toString().replace(/"/g, '""');
+                    // Envolver el valor en comillas si contiene comas o comillas dobles
+                    return escapedValue.includes(',') || escapedValue.includes('"') ? `"${escapedValue}"` : escapedValue;
+                })
+                .join(','); // Usar coma como separador
+        });
+    
+        // Combinar encabezados y datos
+        const csv = [headers, ...csvRows].join('\n');
+        
         // Establecer cabeceras para la descarga
         res.header('Content-Type', 'text/csv');
         res.attachment('datos.csv');
         res.send(csv);
     } catch (error) {
         console.error(error);
-        res.status(500).send('Error al datos');
-    }
+        res.status(500).send('Error datos');
+    }    
 });
 
 module.exports = router;
